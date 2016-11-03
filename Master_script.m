e1dea7e030accd93a236cbf9d659acfb55ff5458
Peter Ferjancic <pferjancic@wisc.edu>
2016-11-03 15:33:29 -0500

    The first commit

---
+++ Master_script.m
@@ -0,0 +1,15 @@
+%% Final project master script
+
+% "One Script to rule them all,
+% one Script to find them,
+% one Script to bring them all
+% and in the Matlab bind them"
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% cd ('E:\005-faks\CS767\CS767_project')
+
+% addpath('E:\005-faks\CS767\CS767_project')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% here come the links to other files
